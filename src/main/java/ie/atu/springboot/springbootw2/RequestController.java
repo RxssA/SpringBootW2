17912d6d3424c05da5c06cc7fff5ccc539a84643
RxssA <g00401655@atu.ie>
2023-10-03 19:03:49 +0100

    added @getMapping and @RequestParam

--- src/main/java/ie/atu/springboot/springbootw2/RequestController.java
+++ src/main/java/ie/atu/springboot/springbootw2/RequestController.java
@@ -3,12 +3,25 @@ package ie.atu.springboot.springbootw2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequestMapping
 public class RequestController {
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name) {
+        return "Your name is " + name;
+    }
+
+    @GetMapping("/details")
+    public String details(@RequestParam String name,
+                          @RequestParam int age){
+        return name + "has just turned" + age;
     }
 
 
